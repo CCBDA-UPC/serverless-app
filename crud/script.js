a302f0aebc59c7c3064aebc2e43c0fda708d0284
angeltoribio-UPC-BCN <atgbcn@gmail.com>
2025-04-16 10:39:54 +0200

    scripts reviewed

--- crud/script.js
+++ crud/script.js
@@ -6,53 +6,50 @@ async function configure() {
 }
 
 (function ($) {
-    var config;
     $.getJSON("variables.json", function (data) {
-        config = data;
-    });
-
-
-    $.ajax({
-        type: 'GET',
-        url: config.url,
-        data: {'TableName': config.table},
-        crossDomain: true,
-        success: function (result) {
-            $.each(result.Items, function (i, item) {
-                $('#items').append('<li>' + item.thingID.S + '</li>');
-            });
-        },
-        error: function (xhr, status, error) {
-            $('#error').toggle().append('<div>' + error + '</div>');
-        }
-    });
-
-    // Form submit
-    $("#form").submit(function (event) {
-        event.preventDefault();
-        thingID = $('#thingID').val();
-        payload = {
-            'TableName': config.table,
-            'Item': {
-                'thingID': {
-                    'S': thingID
-                }
-            }
-        }
+        var config = data;
         $.ajax({
-            type: 'POST',
+            type: 'GET',
             url: config.url,
+            data: {'TableName': config.table},
             crossDomain: true,
-            contentType: 'application/json',
-            data: JSON.stringify(payload),
-            cache: false,
             success: function (result) {
-                $('#thingID').val('');
-                $('#items').append('<li>' + thingID + '</li>');
+                $.each(result.Items, function (i, item) {
+                    $('#items').append('<li>' + item.thingID.S + '</li>');
+                });
             },
             error: function (xhr, status, error) {
-                $('#error').toggle().append('<div>' + status + ',' + error + '</div>');
+                $('#error').toggle().append('<div>' + error + '</div>');
             }
+        });
+
+        // Form submit
+        $("#form").submit(function (event) {
+            event.preventDefault();
+            thingID = $('#thingID').val();
+            payload = {
+                'TableName': config.table,
+                'Item': {
+                    'thingID': {
+                        'S': thingID
+                    }
+                }
+            }
+            $.ajax({
+                type: 'POST',
+                url: config.url,
+                crossDomain: true,
+                contentType: 'application/json',
+                data: JSON.stringify(payload),
+                cache: false,
+                success: function (result) {
+                    $('#thingID').val('');
+                    $('#items').append('<li>' + thingID + '</li>');
+                },
+                error: function (xhr, status, error) {
+                    $('#error').toggle().append('<div>' + status + ',' + error + '</div>');
+                }
+            });
         });
     });
 })(jQuery);
